578029233b58c2b20b9ac5b55d53efdda433065a
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2020-09-19 12:41:48 +0530

    Feed component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/core";
 import theme from "@chakra-ui/theme";
 import Header from "./components/Header";
+import Feed from "./components/Feed";
 
 export const App = () => (
   <ChakraProvider theme={theme} resetCSS portalZIndex={40}>
-    <Header />
+    <Feed />
   </ChakraProvider>
 );
